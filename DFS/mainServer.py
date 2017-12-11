843667a7088ed3627690f9dec9dfbdef5f316f88
Salil Ajgaonkar <ajgaonkar.salil@gmail.com>
2017-12-11 11:13:30 +0000

    got a connection working

--- DFS/mainServer.py
+++ DFS/mainServer.py
@@ -4,17 +4,25 @@ import threading
 
 def main():
     try:
-        server = TCPServer(8000)
+        server = TCPServer()
         server.listen()
     except socket.error, errorMsg:
         print "Failed to create a socket" + str(errorMsg)
 
-class TCPServer(strPort):
-	port=strPort
+class TCPServer(object):
+
+	port = 8000
 	host = "127.0.0.0"
-	def _init_(self):
+
+	def listen(self):
 		self.socketConn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.sock.bind((self.host,port))
+		self.socketConn.bind((self.host,port))
+		self.sock.listen()
+		self.sock.accept()
+		print("connected")
+
+
+
 
 		
 
