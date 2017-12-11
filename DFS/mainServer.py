560209e7b26f3307d0fdf90d09e0694094f88400
Salil Ajgaonkar <ajgaonkar.salil@gmail.com>
2017-12-11 23:13:42 +0000

    Trying to run a transparentFileSystem, some issue with threading

--- DFS/mainServer.py
+++ DFS/mainServer.py
@@ -1,75 +1,36 @@
 import socket
 import threading
+import Queue
 
 class TCPServer(object):
 
-	port = 8000
 	host = "127.0.0.0"
 
-	def _init_(self):
+	def __init__(self, port=None, handler=None):
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.sock.bind((self.host,self.port))
-		#self.handler = self.testHandler
-		thread = ThreadHandler(self)
-		thread.setDaemon(True)
-		thread.start()
+		self.sock.bind((host,port))
+		self.handler = handler
+		self.threadQueue = Queue.Queue(maxsize=15)
+
+        for i in range(15):
+            thread = ThreadHandler(self.threadQueue, 4096, self)
+            thread.setDaemon(True)
+            thread.start()
 
 	def testHandler(self):
 		return True
 
 	def listen(self):
-		self.sock.listen(10)
+		self.sock.listen()
 		self.sock.accept()
 		print("connected")	
 
 class ThreadHandler(threading.Thread):
-    def __init__(self, thread_queue, buffer_length, server):
-        threading.Thread.__init__(self)
-        self.queue = thread_queue
-        self.buffer_length = buffer_length
-        self.server = server
-        self.messageHandler = server.handler
+	def __init__(self,threadQueue,bufferLength,server):
+		threading.Thread.__init__(self)
+		self.server=server
+		self.messageHandler=server.handler
 
-    def run(self):
-        # Thread loops and waits for connections to be added to the queue
-        while True:
-            request = self.queue.get()
-            self.handler(request)
-            self.queue.task_done()
-
-    def handler(self, (con, addr)):
-        message = ""
-        # Loop and receive data
-        while "\n\n" not in message:
-            data = con.recv(self.buffer_length)
-            message += data
-            if len(data) < self.buffer_length:
-                break
-        # If valid http request with message body
-        if len(message) > 0:
-            if message == "KILL_SERVICE\n":
-                print "Killing service"
-                self.server.kill_serv(con)
-            elif re.match(self.server.HELO_REGEX, message):
-                self.server.helo(con, addr, message)
-            elif self.messageHandler(message, con, addr):
-                None
-            else:
-                print message
-                self.server.default(con, addr, message)
-        return
-
-#class connThread(threading.Thread):
-#	def _init_(self, server):
-#		threading.Thread._init_(self)
-#		self.server = server
-#		self.messageHandler = server.testHandler
-
-#	def run(self):
-#		while True:
-#			request = self.queue.get()
-        #self.handler(request)
-        #self.queue.task_done()
 
 def main():
     try:
--- DFS/transparentFileSystem.py
+++ DFS/transparentFileSystem.py
@@ -1,11 +1,24 @@
 import socket
 import threading
+import Queue
+
+from mainServer import TCPServer
 
 def main():
     try:
-        server = TCPServer(8000)
-        server.listen()
+        TFS = TFS(8001)
+        TFS.listen()
     except socket.error, errorMsg:
         print "Failed to create a socket" + str(errorMsg)
+
+class TFS(TCPServer):
+
+    def __init__(self, port_use=None):
+        TCPServer.__init__(self, port_use, self.func)
+
+    def func():
+    	return False;
+
+
 
 if __name__ == "__main__": main()
