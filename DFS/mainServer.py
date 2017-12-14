ef2ee6f0a79ac00f5f3396cd48e76aa76fd1597c
Salil Ajgaonkar <ajgaonkar.salil@gmail.com>
2017-12-14 00:44:10 +0000

    finally got the connection working, able to send messages

--- DFS/mainServer.py
+++ DFS/mainServer.py
@@ -2,34 +2,41 @@ import socket
 import threading
 import Queue
 
+
 class TCPServer(object):
 
-	host = "127.0.0.0"
-
+	host = 'localhost'
 	def __init__(self, port=None, handler=None):
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.sock.bind((host,port))
-		self.handler = handler
+		self.sock.bind((self.host,port))
+
+		self.handler = self.testHandler
 		self.threadQueue = Queue.Queue(maxsize=15)
 
-        for i in range(15):
-            thread = ThreadHandler(self.threadQueue, 4096, self)
-            thread.setDaemon(True)
-            thread.start()
+		for i in xrange(15):
+			thread = ThreadHandler(self.threadQueue, 4096, self)
+			thread.setDaemon(True)
+			thread.start()
 
 	def testHandler(self):
+		print 'YESSSSSS'
 		return True
 
 	def listen(self):
-		self.sock.listen()
-		self.sock.accept()
-		print("connected")	
+		self.sock.listen(5)
+		while True:
+			connection, address = self.sock.accept()
+			self.threadQueue.put((connection, address))
+			print("connected")
 
 class ThreadHandler(threading.Thread):
-	def __init__(self,threadQueue,bufferLength,server):
+
+	def __init__(self, threadQueue, bufferLength, server):
 		threading.Thread.__init__(self)
 		self.server=server
-		self.messageHandler=server.handler
+		self.queue = threadQueue
+		self.bufferLength = bufferLength
+		self.messageHandler = server.handler
 
 	def run(self):
 		while True:
@@ -37,12 +44,21 @@ class ThreadHandler(threading.Thread):
 			self.handler(request)
 			self.queue.task_done()
 
+	def handler(self, (connection, address)):
+		data = connection.recv(self.bufferLength)
+		if len(data) != 0 and len(data) < self.bufferLength:
+			if len(data) > 0:
+				print data
+		return
+
 
 def main():
     try:
-        server = TCPServer()
+        server = TCPServer(8001)
         server.listen()
     except socket.error, errorMsg:
         print "Failed to create a socket" + str(errorMsg)
+
+
 
 if __name__ == "__main__": main()
